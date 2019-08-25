d7427bab7628c3760a40fc681ad334a426941e42
Josh <joshuah.m.martin@gmail.com>
2019-08-24 22:16:00 -0400

    edited code for better readability

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,16 +2,22 @@ let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
 
 function checkForMatch() {
+
 	if (cardsInPlay.length === 2) {
+
 		if (cardsInPlay[0] === cardsInPlay[1]) {
+
 			alert("You found a match!");
+
 		} else {
+			
 			alert("Sorry, try again.");
 		}
 	}
 };
 
 function flipCard(cardId) {
+
 	console.log("User flipped " + cards[cardId]);
 
 	cardsInPlay.push(cards[cardId]);
